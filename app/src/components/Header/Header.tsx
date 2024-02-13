e7ce9ef0ca5a805a70bed01c2f4f8f0fc3a0f484
Jakub Kucera <kucerj64@fit.cvut.cz>
2024-02-13 22:10:52 +0100

    feat: Add timer functionality and audio notifications

--- app/src/components/Header/Header.tsx
+++ app/src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import "./Header.css";
 
-export default function Header({ text }: { text: string }) {
+export default function Header({ text }: { text: string | undefined }) {
     return (
         <header className="header--container">
             <h2 className="header--text">{text}</h2>
--- app/src/components/Timer/Timer.tsx
+++ app/src/components/Timer/Timer.tsx
@@ -1,8 +1,29 @@
 import "./Timer.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-export default function Timer() {
-    const [time] = useState(123124);
+export default function Timer({ status }: { status: string }) {
+    /* Display correct time. */
+
+    const [time, setTime] = useState<number>(0);
+
+    // Fetch the correct time
+    useEffect(() => {
+        chrome.storage.local.get(["time"], (result) => {
+            setTime(result.time);
+        });
+    }, []);
+
+    // Listens for updateTime messages
+
+    useEffect(
+        () =>
+            chrome.runtime.onMessage.addListener((request: { action: string; time: number }) => {
+                console.log("Timer message received", request);
+
+                if (request.action === "updateTime") setTime(request.time);
+            }),
+        []
+    );
 
     const formatTime = (time: number) => {
         const minutes: number = Math.floor(time / 1000 / 60);
@@ -12,10 +33,30 @@ export default function Timer() {
 
     const timeDisplay = formatTime(time);
 
+    const handleTimerClick = () => chrome.runtime.sendMessage({ action: "handleTimerClick" });
+
+    /* Apply correct styling */
+
+    const [style, setStyle] = useState<object>({});
+
+    // Fetch colors and set appropriate colors
+    useEffect(() => {
+        chrome.storage.local.get(["colors"], (result) => {
+            const currentColor = (result.colors as { [key: string]: string })[status];
+
+            setStyle(() => ({
+                background: `${currentColor}`,
+                boxShadow: `0px 0px 10px ${currentColor}`,
+            }));
+        });
+    }, [status]);
+
+    console.log("Timer Rendered", time, status);
+
     return (
         <div className="timer--container">
-            <button className="timer--button">
-                <div className="timer--outer-circle"></div>
+            <button className="timer--button" onClick={handleTimerClick}>
+                <div style={style} className="timer--outer-circle"></div>
                 <div className="timer--inner-circle"></div>
                 <h2 className="timer--time">{timeDisplay}</h2>
             </button>
